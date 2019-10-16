05bd10ef815df68ac69db340cba786ec6c300ae1
Bob Caldwell <themaccat@gmail.com>
2019-10-16 18:07:48 -0400

    unsure

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -1,18 +1,19 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDEg3j6cDtsaY4L1nnbm1zMh99IuYmTq6E",
-    authDomain: "employeetracker-62a4c.firebaseapp.com",
-    databaseURL: "https://employeetracker-62a4c.firebaseio.com",
-    projectId: "employeetracker-62a4c",
-    storageBucket: "employeetracker-62a4c.appspot.com",
-    messagingSenderId: "215024302088",
-    appId: "1:215024302088:web:3f723a50e5b7356ae339bd"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+  apiKey: "AIzaSyDEg3j6cDtsaY4L1nnbm1zMh99IuYmTq6E",
+  authDomain: "employeetracker-62a4c.firebaseapp.com",
+  databaseURL: "https://employeetracker-62a4c.firebaseio.com",
+  projectId: "employeetracker-62a4c",
+  storageBucket: "employeetracker-62a4c.appspot.com",
+  messagingSenderId: "215024302088",
+  appId: "1:215024302088:web:3f723a50e5b7356ae339bd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-
-  database.ref().on("child_added", function(snapshot) {
+database.ref().on(
+  "child_added",
+  function(snapshot) {
     // console.log(snapshot);
     var sv = snapshot.val();
 
@@ -24,42 +25,46 @@ var firebaseConfig = {
     // TODO call calcMonthsWorked
     calcMonthsWorked(sv.startDate);
     // $("#eName").prepend(sv.eName);
-  
-  // If any errors are experienced, log them to console.
-  }, function(errorObject) {
+
+    // If any errors are experienced, log them to console.
+  },
+  function(errorObject) {
     console.log("The read failed: " + errorObject.code);
-  });
-  
-  function calcMonthsWorked(dateVar) {
-    console.log(dateVar);
   }
-  
-  
+);
 
+function calcMonthsWorked(dateVar) {
+  console.log(dateVar);
+  var ourFormat = "MM/DD/YYYY";
+  var OurDate = moment(dateVar,ourFormat);
 
+  var duration = moment.duration(end.diff(dateVar, "months"));
 
+  console.log();
 
-  $("#add-employee-btn").on("click", function(event) {
-    event.preventDefault();
-    // Get the input values
-    var eName = $("#employee-name-input").val().trim(); 
-    var role = $("#role-input").val().trim();
-    var startDate = ($("#start-input").val().trim());
-    var rate = ($("#rate-input").val().trim());
-  
-  
-  
-      database.ref().push({
-        eName: eName,
-        role: role,
-        startDate: startDate,
-        rate: rate,
-      });
-  
-    
-    });
-  
-  
- 
-  
+  console.log("The months worked: " + duration);
+}
 
+$("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+  // Get the input values
+  var eName = $("#employee-name-input")
+    .val()
+    .trim();
+  var role = $("#role-input")
+    .val()
+    .trim();
+  var startDate = $("#start-input")
+    .val()
+    .trim();
+  var rate = $("#rate-input")
+    .val()
+    .trim();
+
+  database.ref().push({
+    eName: eName,
+    role: role,
+    startDate: startDate,
+    rate: rate
+  });
+});
